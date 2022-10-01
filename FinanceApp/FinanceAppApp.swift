908bf34e752a922d075e16abb9b7436d1f1f8415
Radu Maftei <mafteiradu23@yahoo.com>
2022-10-01 19:29:27 +0300

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Radu Maftei on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
